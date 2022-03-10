9516a9354a46b7ba9a50c3c37b0d2e34c204c6a0
Marco Brianza <marcobrianza@gmail.com>
2022-03-10 14:12:52 +0100

    real 1.7.2

---
+++ Arduino/WeMakeColorsII/_info.h
@@ -0,0 +1,38 @@
+boolean DEBUG_INFO = true;
+
+#include <ESP8266WiFi.h>  // built in ESP8266 Core
+
+String softwareName = "WeMakeColorsII";
+String softwareVersion = "1.7.3";
+String appId = "WMCII";
+
+String softwareInfo = "";
+String softwarePlatform = "";
+
+String thingId = "";
+String name = "";
+
+void softwareInfo_setup(String n) {
+  Serial.begin(115200);
+  
+  if (n == "") name = thingId;
+  else   name = n;
+
+  softwareInfo = softwareName + " - " + softwareVersion +  " - " + ESP.getSketchMD5() + " - " + String (ESP.getCpuFreqMHz()); // + " - " + String (__DATE__) + " - " + String(__TIME__);;
+  softwarePlatform = ESP.getFullVersion();
+  thingId = appId + "_" +  WiFi.macAddress().c_str();
+  name = thingId;
+
+
+
+  if (DEBUG_INFO) {
+    Serial.println("\n");
+    Serial.println(softwareInfo);
+    Serial.println(softwarePlatform);
+    Serial.println("resetReason=" + ESP.getResetReason());
+    Serial.println("thingId: " + thingId);
+    Serial.println("name: " + name);
+    Serial.flush();
+  }
+
+}
